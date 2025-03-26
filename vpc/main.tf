c1ee3f9d2ad443f74164255c7d1ce66f8a7d91c2
maheshforce <90966230+maheshforce@users.noreply.github.com>
2025-03-26 12:58:40 +0530

    Delete vpc directory

--- vpc/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mahesh7893" # S3 bucket name
-    key            = "vpc/terraform.tfstate" # State file path within the bucket
-    region         = "us-east-1"                 # Bucket region
-    encrypt        = true                        # Encrypt the
-  }
-}
